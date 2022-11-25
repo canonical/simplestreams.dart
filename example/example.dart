f9b44a478844cb5e8ca33371592d306130adb09b
J-P Nurmi <jp.nurmi@canonical.com>
2022-11-25 13:56:19 +0100

    Example snippet

--- example/example.dart
+++ example/example.dart
@@ -1,10 +1,3 @@
-# Simple Streams client for Dart
-
-[Simple Streams](https://launchpad.net/simplestreams/) describe streams of like
-items in a structural fashion. A client provides a way to sync or act on changes
-in a remote stream.
-
-```dart
 import 'package:simplestreams/simplestreams.dart';
 
 Future<void> main() async {
@@ -24,4 +17,3 @@ Future<void> main() async {
 
   client.close();
 }
-```
